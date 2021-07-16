84332be5112ca07728a900b3235cbaf16ba69d14
tom_glb <hsms_glb@qq.com>
2021-07-16 16:41:59 +0800

    第3次提交

--- src/main/java/com/hsms/git/GitTest.java
+++ src/main/java/com/hsms/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("the first test");
         System.out.println("the first test");
+        System.out.println("the test3");
     }
 }
